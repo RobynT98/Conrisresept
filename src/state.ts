67da6cb3e796199c374c2708a3df49eb06381d91
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-11-01 17:18:55 +0100

    Create src/state.ts

---
+++ src/state.ts
@@ -0,0 +1,19 @@
+export const defaultChapters = [
+  "Förrätter",
+  "Huvudrätter",
+  "Desserter",
+  "Drycker",
+  "Såser"
+];
+
+export const defaultThemes = [
+  "Vardagsfavorit",
+  "Höst",
+  "Högtid",
+  "Vegetariskt",
+  "Snabbt"
+];
+
+export function uuid() {
+  return crypto.randomUUID();
+}
